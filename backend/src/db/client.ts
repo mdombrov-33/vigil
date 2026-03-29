22058147b6e4846926fc719627d8674aca714765
Maksym Dombrov <maksymdombrov@gmail.com>
2026-03-29 18:00:37 +0300

    fix: fix leftover sql connections on GCP

--- backend/src/db/client.ts
+++ backend/src/db/client.ts
@@ -3,5 +3,5 @@ import postgres from "postgres";
 import * as schema from "./schema.js";
 import * as enums from "./enums.js";
 
-const conn = postgres(process.env.DB_URL!);
+const conn = postgres(process.env.DB_URL!, { max: 5 });
 export const db = drizzle(conn, { schema: { ...schema, ...enums } });
